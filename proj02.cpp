abd94665b5b40acf894fcc8220378eb740dec97e
Matt London <mlondon2@uccs.edu>
2011-10-31 22:54:31 -0600

    Made some minor changes to the format of the output and the format of the code

--- proj02.cpp
+++ proj02.cpp
@@ -14,69 +14,72 @@ sem_t mutex;
 
 int content[MAX];
 int count;
-int eValue;
-int oValue;
-int dValue;
+//For Debugging...
+//int eValue;
+//int oValue;
+//int dValue;
 
 void doSwap(int l, int r)
 {
-  sem_wait(&mutex);                                                 //Lock the C-Region
-  cout << "Entering C-Region..." << endl;
-  if(content[l] > content[r])
-  {
-    cout << "Swapping " << content[l] << " with " << content[r] << endl;
-    int tmp = content[l];
-    content[l] = content[r];
-    content[r] = tmp;
-  }
-  cout << "Leaving C-Region..." << endl;
-  sem_post(&mutex);                                                 //Unlock the C-Region
-  sem_post(&donePhase);                                             //Increment Phase Done
+    sem_wait(&mutex);                                                 //Lock the C-Region
+    cout << "Entering C-Region..." << endl;
+    if(content[l] > content[r])
+    {
+        cout << "Swapping " << content[l] << " with " << content[r] << endl;
+	int tmp = content[l];
+	content[l] = content[r];
+	content[r] = tmp;
+    }
+    cout << "Leaving C-Region..." << endl;
+    sem_post(&mutex);                                                 //Unlock the C-Region
+    sem_post(&donePhase);                                             //Increment Phase Done
 }
 
 void displayContents()
 {
-  for(int i = 0; i < count; i++)
-  {
-    cout << content[i] << endl;
-  }  
+    cout << "File Contents:" << endl;
+    for(int i = 0; i < count; i++)
+    {
+        cout << content[i] << " ";
+    }  
+    cout << endl;
 }
 
 void *sortContent(void *tid)
 {
-  int id = (long)tid;
-  for(int i = 0; i < count; i++)
-  {
-    if(((i + 1) % 2) != 0)                                            //Odd Phase
+    int id = (long)tid;
+    for(int i = 0; i < count; i++)
     {
-      int l = (2 * id);                                             //left index always even; l = 2K
-      int r = l + 1;
-      sem_wait(&oddWait);
-      cout << "Odd " << id << ": executing... " << endl;
-      doSwap(l, r);
-      sem_post(&evenWait);
+        if(((i + 1) % 2) != 0)                                            //Odd Phase
+	{
+	    int l = (2 * id);                                             //left index always even; l = 2K
+	    int r = l + 1;
+	    sem_wait(&oddWait);
+	    cout << "Odd " << id << ": executing... " << endl;
+	    doSwap(l, r);
+	    sem_post(&evenWait);
+	}
+	else                                                            //Even Phase
+        {  
+	    if(id != 1)                                                   //Skip the last thread in the even phase (N/2)
+	    {	                                            
+	        int l = (2 * id) + 1;                                       //left index always odd: l = 2K + 1
+		int r = l + 1;
+		sem_wait(&evenWait);
+		cout << "Even " << id << ": executing... " << endl;
+		doSwap(l, r);
+		sem_post(&oddWait);
+	    }
+	}
+	//For Debugging
+	//sem_getvalue(&evenWait, &eValue)
+	//sem_getvalue(&oddWait, &oValue)
+	//sem_getvalue(&donePhase, &dValue)
+	//cout << "Even: " << eValue << 
+	//endl << "Odd:  " << oValue << 
+	//endl << "Done: " << dValue <<
+	//endl;
     }
-    else                                                            //Even Phase
-    {
-      if(id != 1)                                                   //Skip the last thread in the even phase (N/2)
-      {	                                            
-	int l = (2 * id) + 1;                                       //left index always odd: l = 2K + 1
-	int r = l + 1;
-	sem_wait(&evenWait);
-	cout << "Even " << id << ": executing... " << endl;
-	doSwap(l, r);
-	sem_post(&oddWait);
-      }
-    }  
-    //sem_getvalue(&evenWait, &eValue)
-    //sem_getvalue(&oddWait, &oValue)
-    //sem_getvalue(&donePhase, &dValue)
-    //cout << "Even: " << eValue << 
-    //endl << "Odd:  " << oValue << 
-    //endl << "Done: " << dValue <<
-    //endl;
-  }
-  //  displayContents();
 }
 
 int readFile(char *fileName)
@@ -91,7 +94,7 @@ int readFile(char *fileName)
 	  content[count++] = value;      
 	}
     } else {
-      cout << "Invalid!" << endl;
+      cout << "Invalid file name or directory!" << endl;
     }
   return count;
 }
@@ -106,7 +109,7 @@ int main(int argc, char *argv[])
 	int noWorkers = (count / 2);
 	pthread_t worker[noWorkers];
 	
-	sem_init(&donePhase, 0, 0);
+	sem_init(&donePhase, 0, noWorkers);
 	sem_init(&oddWait, 0, noWorkers);
 	sem_init(&evenWait, 0, 0);
 	sem_init(&mutex, 0, 1);
